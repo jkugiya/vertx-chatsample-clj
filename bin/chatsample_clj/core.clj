5e3ba3c6e19fe7384f6edfd85a9f32a5ebc14228
jkugiya <j.kugiya@gmail.com>
2014-06-09 06:04:07 +0900

    Initial Commit

---
+++ bin/chatsample_clj/core.clj
@@ -0,0 +1,23 @@
+(ns chatsample-clj.core
+  (:require [vertx.core :as vertx]
+            [vertx.net :as net]
+            [vertx.stream :as stream]
+            [vertx.eventbus :as eb]))
+(def ^:dynamic **broadcast-address** "broadcast-address")
+(defn -connect-handler [sock]
+  (let [message-handler (fn [event] (stream/write sock event))
+        remote-addr (net/remote-address sock)
+        addr-info (format "%s:%d"  (:host remote-addr) (:port remote-addr))
+        data-handler (fn [data] (let [now (java.time.ZonedDateTime/now)]
+                                      (eb/publish **broadcast-address**
+                                                  (format "%s<%s> %s" now addr-info data))))]
+    (eb/on-message **broadcast-address** message-handler)
+    (-> sock
+      (stream/write (format "Welcome to the chat %s" addr-info))
+      (stream/on-data data-handler)
+      (net/on-close #(eb/unregister-handler **broadcast-address** message-handler)))))
+(defn init[]
+  (let [server (net/server)]
+    (-> server
+      (net/on-connect -connect-handler)
+      (net/listen 1234 "localhost"))))
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject chatsample-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.vertx/clojure-api "1.0.2"]]
+  :plugins [[lein-vertx "0.3.0-SNAPSHOT"]]
+  :vertx {:main chatsample-clj.core/init
+          :author "FIXME: Your name"
+          :keywords ["FIXME: keywords"]
+          :developers ["FIXME: other developers"]})
